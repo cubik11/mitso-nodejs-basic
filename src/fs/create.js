228d33a02011b7edd2357bc456e79d96b1ff163c
cubik11 <cubik4236@gmail.com>
2024-03-16 11:41:49 +0300

    test

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,4 +1,18 @@
 const create = async () => {
+    try {
+    // Путь и имя нового файла
+    const filePath = 'path/to/newfile.txt';
+
+    // Содержимое нового файла
+    const fileContent = 'Привет, мир!';
+
+    // Создание файла
+    await fs.promises.writeFile(filePath, fileContent);
+
+    console.log('Новый файл успешно создан.');
+  } catch (error) {
+    console.error('Ошибка при создании файла:', error);
+  }
     // Write your code here 
 };
 
