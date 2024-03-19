25a9004fcdad006a1329ca391ce82f6a0e1de083
cubik11 <cubik4236@gmail.com>
2024-03-19 13:35:03 +0300

    rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,33 @@
+import fs from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        // Check if the files already exist
+        const files = await Promise.all([
+            fs.access('/home/vika/Рабочий стол/files_copy/wrongFilename.txt'),
+            fs.access('/home/vika/Рабочий стол/files_copy/properFilename.md')
+          ]);
+
+        // If files exist, throw an error
+        throw new Error('FS operation failed');
+
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            console.error(error.message);
+            return;
+        }
+
+        // If files don't exist, proceed with renaming
+        try {
+            await fs.rename(
+                '/home/vika/Рабочий стол/files_copy/wrongFilename.txt',
+                '/home/vika/Рабочий стол/files_copy/properFilename.md'
+              );
+              console.log('Files renamed successfully');
+        } catch (error) {
+            console.error('FS operation failed');
+        }
+    }
 };
 
 await rename();
