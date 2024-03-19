eff86b8f54732e73c9b4b5e25e70218add5a98ed
cubik11 <cubik4236@gmail.com>
2024-03-19 13:54:52 +0300

    delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const filePath = '/home/vika/Рабочий стол/fresh.txt';
+
+    await fs.access(filePath);
+    await fs.unlink(filePath);
+
+    console.log('File removed successfully');
+  } catch (error) {
+    console.error('FS operation failed:', error.message);
+  }
 };
 
 await remove();
