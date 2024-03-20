ccea1593796bda5cafe65ea4825b978f64a9ee2d
cubik11 <cubik4236@gmail.com>
2024-03-20 10:09:44 +0300

    read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const filePath = '/home/vika/Рабочий стол/fileToRead.txt';
+
+    const content = await fs.readFile(filePath, 'utf-8');
+    console.log('File content:');
+    console.log(content);
+  } catch (error) {
+    console.error('FS operation failed:', error.message);
+  }
 };
 
 await read();
