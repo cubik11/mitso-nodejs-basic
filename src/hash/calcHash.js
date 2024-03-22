7f920cf636d9523a2413c1d153ebca6919e10862
cubik11 <cubik4236@gmail.com>
2024-03-22 14:05:44 +0300

    hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
+
+  const fileData = await fs.promises.readFile(filePath);
+
+  const hash = crypto.createHash('sha256');
+  hash.update(fileData);
+  const hexHash = hash.digest('hex');
+
+  console.log(hexHash);
 };
 
 await calculateHash();
