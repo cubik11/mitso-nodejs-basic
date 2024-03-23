cd2f20cce1f312884739d1d7639cc7a0e4f27a9f
cubik11 <cubik4236@gmail.com>
2024-03-23 12:54:44 +0300

    transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
     // Write your code here 
+    let stream = new Transform({
+        transform(chunk, encoding, callback) {
+            this.push(chunk.toString().split('').reverse().join(''));
+            callback();
+        }
+    });
+    process.stdin.pipe(stream).pipe(process.stdout);
 };
 
 await transform();
