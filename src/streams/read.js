07258ddc90455a6dad1accc2a8adf339dc20701a
cubik11 <cubik4236@gmail.com>
2024-03-23 11:55:19 +0300

    read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = '/home/vika/Рабочий стол/mitso-nodejs-basic/src/streams/files/fileToRead.txt';
+
+  const stream = fs.createReadStream(filePath, { encoding: 'utf8' });
+
+  stream.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  stream.on('end', () => {
+    console.log('Чтение файла завершено.');
+  });
+
+  stream.on('error', (error) => {
+    console.error('Ошибка при чтении файла:', error);
+  });
 };
 
 await read();
