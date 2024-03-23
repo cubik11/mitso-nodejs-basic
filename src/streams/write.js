19bbcf1947b507df08c21b4ce323144dc457a215
cubik11 <cubik4236@gmail.com>
2024-03-23 12:14:50 +0300

    write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+
 const write = async () => {
     // Write your code here 
+    let stream = fs.createWriteStream('/home/vika/Рабочий стол/mitso-nodejs-basic/src/streams/files/fileToWrite.txt');
+    process.stdin.on('data', (chunk) => {
+        if (chunk.toString().trim() === 'exit') {
+            stream.end();
+            process.stdin.pause();
+            return;
+        }
+        stream.write(chunk);
+    });
 };
 
 await write();
